0372f0f345e0a00403451d270f99f7f683e507a6
Marcelo <guedesmarcello733@outlook.com>
2024-06-03 21:11:13 -0300

    Adicionando o Insert no repo do cachorro

---
+++ src/main/java/br/unipar/repositories/CachorroRepository.java
@@ -0,0 +1,7 @@
+package br.unipar.repositories;
+
+public class CachorroRepository {
+    private static final String INSERT =
+            "INSERT INTO CACHORRO(nome, vl_tamanho, st_perfume, dt_nascimento, id_raca, id_pelagem, id_cor) VALUES(?, ?, ?, ?, ?, ?, ?)";
+
+}
