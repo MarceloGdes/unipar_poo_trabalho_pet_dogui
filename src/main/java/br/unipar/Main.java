87675220d3408a86d234dcb355a2df21e5d1511c
Rafael Gustavo Maccari <130612737+rgmaccari@users.noreply.github.com>
2024-06-10 00:39:35 -0300

    att main (inclusao pelagem)

--- src/main/java/br/unipar/Main.java
+++ src/main/java/br/unipar/Main.java
@@ -87,6 +87,32 @@ public class Main {
             // LISTAGEM DE RAÇA APÓS EXCLUSÃO:
             System.out.println("Raças: " + racaService.findAll());
 
+            //Pelagem:
+            PelagemService pelagemService = new PelagemService();
+            
+            Pelagem pelagem1 = new Pelagem();
+            pelagem1.setDesc("Pelo curto e liso.");
+            System.out.println("Insert" + pelagemService.insert(pelagem1));
+            
+            Pelagem pelagem2 = new Pelagem();
+            pelagem2.setDesc("Pelo curto e duro.");
+            System.out.println("Insert: " + pelagemService.insert(pelagem2));
+            
+            Pelagem pelagem3 = new Pelagem();
+            pelagem3.setDesc("Pelo longo e liso.");
+            System.out.println("Insert: " + pelagemService.insert(pelagem3));
+            
+            System.out.println("Pelagens: " + pelagemService.findAll());
+            
+            Pelagem pelagemAlteracao = pelagemService.findById(1);
+            pelagemAlteracao.setDesc("Pelo longo ondulado.");
+            System.out.println("Update: " + pelagemAlteracao.toString());
+            
+            int idPelagemDelete = 1;
+            System.out.println("Delete: " + pelagemService.findById(idPelagemDelete));
+            pelagemService.delete(idPelagemDelete);
+            
+            System.out.println(pelagemService.findAll());
 
 
         }catch (SQLException e){
